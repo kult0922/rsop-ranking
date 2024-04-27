6d26ebb21001d4281a668fe76210262717e7c37b
kurisaki_kazuma <38486600+kult0922@users.noreply.github.com>
2024-04-27 13:47:53 +0900

    set up d1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,25 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+interface Env {
+  DB: D1Database;
+}
+
+type User = {
+  id: number;
+  username: string;
+};
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const env = context.env as Env;
+
+  const { results } = await env.DB.prepare("SELECT * FROM users").all<User>();
+
+  return json({
+    users: results ?? [],
+  });
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +29,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { users } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      {users.map((user) => (
+        <li key={user.id}>{user.username}</li>
+      ))}
     </div>
   );
 }
---
+++ migrations/0001_users.sql
@@ -0,0 +1,5 @@
+-- Migration number: 0001 	 2024-04-27T04:36:53.505Z
+CREATE TABLE `users` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` TEXT NOT NULL
+);
