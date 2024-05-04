a0a2db9adc1e42c15fbdf3754180c7f9d6d78be9
kurisaki_kazuma <38486600+kult0922@users.noreply.github.com>
2024-05-05 01:40:11 +0900

    home design

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,11 +5,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ResponsiveLine } from "@nivo/line";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
 import { User, Game, BBChange } from "~/schema/db";
+import {
+  LineChart,
+  Line,
+  Legend,
+  Tooltip,
+  YAxis,
+  XAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 interface Env {
   DB: D1Database;
@@ -35,6 +46,9 @@ export async function loader({ context }: LoaderFunctionArgs) {
 
   // 重複を削除してユーザIUを取得
   const participantUserIds = new Set(bbChange.map((bb) => bb.user_id));
+  const participantUsers = users.filter((user) =>
+    participantUserIds.has(user.id)
+  );
   const heldGameIds = new Set(bbChange.map((bb) => bb.game_id));
 
   const heldGames = games.filter((game) => heldGameIds.has(game.id));
@@ -58,34 +72,71 @@ export async function loader({ context }: LoaderFunctionArgs) {
   for (const [gameId, bbChanges] of gameId2Users) {
     gameResults.push({
       gameId,
-      bbChanges,
+      bbChanges: bbChanges.sort((a, b) => b.value - a.value),
       gameName: games.find((game) => game.id === gameId)?.name,
       date: games.find((game) => game.id === gameId)?.date,
     });
   }
 
-  const data = [];
+  const dataByUsers: any[] = [];
 
   for (const userId of participantUserIds) {
     const user = users.find((user) => user.id === userId);
     if (!user) continue;
     let acc = 0;
-    const userValues = [{ x: "", y: acc }];
+    const userValues = new Map<number, { acc: number; gameName: string }>();
     for (const game of heldGames) {
       const bb = gameId2Users.get(game.id)!.find((bb) => bb.userId === userId);
-      console.log(user.name, "bb", bb);
-      acc += bb?.value ?? 0;
-      userValues.push({ x: game.name, y: acc });
+      if (!bb) continue;
+      acc += bb.value;
+      userValues.set(game.id, { acc, gameName: game.name });
     }
-    data.push({
-      id: user.name,
+    dataByUsers.push({
+      name: user.name,
+      id: user.id,
       data: userValues,
     });
   }
 
+  const data = [];
+  // すべての参加者のグラフの一番初めに0を打つ
+  const initPoint: any = { name: "" };
+  for (const user of participantUsers) {
+    initPoint[user.name] = 0;
+  }
+  data.push(initPoint);
+
+  for (const game of heldGames) {
+    const point: any = { name: game.name };
+    for (const user of dataByUsers) {
+      const value = user.data.get(game.id);
+      if (value) {
+        point[user.name] = value.acc;
+      }
+    }
+    data.push(point);
+  }
+
+  const currentResult: { name: string; value: number }[] = [];
+  for (const user of participantUsers) {
+    const lastGame = heldGames[heldGames.length - 1];
+    const bb = gameId2Users
+      .get(lastGame.id)!
+      .find((bb) => bb.userId === user.id);
+    if (bb) {
+      currentResult.push({
+        name: user.name,
+        value: bb.value,
+      });
+    }
+  }
+  currentResult.sort((a, b) => b.value - a.value);
+
   return json({
     data,
     gameResults,
+    currentResult,
+    participantUsers,
   });
 }
 
@@ -96,132 +147,147 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const rankSufix = (rank: number) => {
+  if (rank === 1) {
+    return "st";
+  } else if (rank === 2) {
+    return "nd";
+  } else if (rank === 3) {
+    return "rd";
+  } else {
+    return "th";
+  }
+};
+
+const rankIcon = (rank: number) => {
+  if (rank === 1) {
+    return "🥇";
+  } else if (rank === 2) {
+    return "🥈";
+  } else if (rank === 3) {
+    return "🥉";
+  } else {
+    return " ";
+  }
+};
+
 export default function Index() {
-  const { data, gameResults } = useLoaderData<typeof loader>();
+  const { gameResults, data, participantUsers, currentResult } =
+    useLoaderData<typeof loader>();
+
+  const colors = [
+    "#ff595e",
+    "#ff924c",
+    "#ffca3a",
+    "#8ac926",
+    "#52a675",
+    "#1982c4",
+    "#6a4c93",
+    "#4267ac",
+  ];
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>RSOP</h1>
-      <div
-        style={{
-          width: "100%",
-          height: "500px",
-        }}
-      >
-        <ResponsiveLine
-          data={data}
-          colors={{ scheme: "dark2" }}
-          theme={{
-            tooltip: {
-              container: {
-                color: "#000",
-              },
-            },
-            legends: {
-              text: {
-                fill: "#fff",
-                fontSize: 12,
-              },
-            },
-            axis: {
-              ticks: {
-                text: {
-                  fill: "#fff",
-                },
-              },
-              legend: {
-                text: {
-                  fill: "#fff",
-                },
-              },
-            },
-          }}
-          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-          xScale={{ type: "point" }}
-          yScale={{
-            type: "linear",
-            stacked: false,
-            min: "auto",
-            max: "auto",
-          }}
-          yFormat=" >-.2f"
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "stack",
-            legendOffset: 36,
-            legendPosition: "middle",
-            truncateTickAt: 0,
-          }}
-          axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "BB",
-            legendOffset: -40,
-            legendPosition: "middle",
-            truncateTickAt: 0,
-          }}
-          pointSize={10}
-          pointColor={{ theme: "background" }}
-          pointBorderWidth={2}
-          pointBorderColor={{ from: "serieColor" }}
-          pointLabelYOffset={0}
-          enableTouchCrosshair={true}
-          useMesh={true}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: "left-to-right",
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: "circle",
-              symbolBorderColor: "rgba(0, 0, 0, .5)",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemBackground: "rgba(0, 0, 0, .03)",
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
-        />
+      <div className="flex justify-between mt-3">
+        <h1 className="text-2xl ml-2">♠ RSOP</h1>
+        <Link to="/new-game" className="mr-2">
+          <Button variant="outline">new game</Button>
+        </Link>
       </div>
-      <div>
-        <Link to="/new-game">new game</Link>
+
+      <Separator className="my-2" />
+
+      <div className="flex justify-center my-4">
+        <Card className="w-3/4">
+          <CardHeader>
+            <CardTitle>Ranking</CardTitle>
+            <CardDescription>Total BB</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {currentResult.map((result, index) => (
+              <div key={result.name} className="flex m-2">
+                <div className="w-16 flex">
+                  <div className="w-6">{rankIcon(index + 1)}</div>
+                  {index + 1}
+                  {rankSufix(index + 1)}
+                </div>
+                <div className="w-16">{result.name}</div>
+                {result.value > 0 ? (
+                  <div className="text-green-500">+{result.value}</div>
+                ) : (
+                  <div className="text-red-500">{result.value}</div>
+                )}
+              </div>
+            ))}
+          </CardContent>
+        </Card>
       </div>
+      <h2 className="text-xl ml-2 my-4 ">History</h2>
+      <Separator className="my-3" />
+
+      <div className="flex justify-center my-4">
+        <Card className="w-[94%] h-96">
+          <CardHeader>
+            <CardTitle>BB faise and fall</CardTitle>
+          </CardHeader>
+          <CardContent className="w-full h-[90%]">
+            <ResponsiveContainer width="100%">
+              <LineChart height={400} data={data}>
+                {participantUsers.map((user, index) => (
+                  <Line
+                    key={`line-${index}`}
+                    dot={{ fill: colors[index], r: 3 }}
+                    type="monotone"
+                    dataKey={user.name}
+                    stroke={colors[index]}
+                    strokeWidth={2}
+                  />
+                ))}
+                <CartesianGrid />
+                <YAxis />
+                <XAxis dataKey="name" />
+                <Legend />
+                <Tooltip
+                  contentStyle={{
+                    backgroundColor: "#000",
+                    border: "none",
+                  }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
+      </div>
+
       {gameResults.map((gameResult) => (
-        <>
-          <Link to={`/games/${gameResult.gameId}`}>
-            <Card>
+        <Link to={`/games/${gameResult.gameId}`}>
+          <div className="flex justify-center m-2">
+            <Card className="w-3/4">
               <CardHeader>
                 <CardTitle>{gameResult.gameName}</CardTitle>
                 <CardDescription>{gameResult.date}</CardDescription>
               </CardHeader>
               <CardContent>
-                {gameResult.bbChanges.map((bbChange) => (
-                  <div key={bbChange.userId}>
-                    {bbChange.userName}
-                    <span>{bbChange.value}</span>
+                {gameResult.bbChanges.map((bbChange, index) => (
+                  <div className="flex" key={bbChange.userId}>
+                    <div className="w-8">
+                      {index + 1}
+                      {rankSufix(index + 1)}
+                    </div>
+                    <div className="ml-2 w-20">{bbChange.userName}</div>
+                    {bbChange.value > 0 ? (
+                      <div className="ml-2 text-green-500">
+                        +{bbChange.value}
+                      </div>
+                    ) : (
+                      <div className="ml-2 text-red-500">{bbChange.value}</div>
+                    )}
                   </div>
                 ))}
               </CardContent>
             </Card>
-          </Link>
-        </>
+          </div>
+        </Link>
       ))}
     </div>
   );
